e92eb8a880579931f7cf52b1a9be103d8106832b
ewstr <erick.rwf@gmail.com>
2021-09-23 11:53:52 -0400

    update scroll feature now is implemented with js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,64 @@
+
 const blocks = document.getElementsByClassName('block');
 const newUl = document.createElement('ul');
 document.querySelector('nav').appendChild(newUl);
+const newLi = document.createElement('li');
 
+// create new menu links based on block sections
 function createLinks() {
-// create a new menu link based on block sections
-    for ( const i of blocks) {
+    for ( const i of blocks ) {
         const newLi = document.createElement('li');
-        // grab link's names from each block
+        // grab links' names from each block
         let menuName = i.dataset.blocksec;
-        newLi.innerHTML = `<a href="#${i.id}">${menuName}</a>`;
+        newLi.innerHTML = `<a href="javascript:void(0)}">${menuName}</a>`;
         document.querySelector('ul').appendChild(newLi); 
     }
 }
 createLinks();
 
-const lis = document.querySelectorAll('li');
+const selectAs = document.querySelectorAll("a");
 
-for (const item of lis) {
-   item.addEventListener('click',  function() {
-   for (const i of lis) {
-    i.classList.remove('active');
-    }
-    item.classList.add('active');
+// create class attribute
+function createClassAtt() {
+  for ( const i of selectAs) {
+  	const att = document.createAttribute("class");
+  	att.value = "scroll_to";
+  	i.setAttributeNode(att);
+  }
+}
+createClassAtt();
+
+// create custom data attribute
+function createDataAtt() {
+  for (let i = 0; i < selectAs.length; i++) {
+      let item = selectAs[i];
+      item.dataset.link = `section${i + 1}`;
+  }
+}
+createDataAtt();
+
+// smooth scrooll motion
+function scroollMotion() {
+  const links = document.querySelectorAll('.scroll_to');
+  links.forEach((item) =>{
+    item.addEventListener('click', ()=>{
+      const el = document.getElementById(item.getAttribute('data-link'));
+      el.scrollIntoView({behavior:'smooth'});
+    })
   })
 }
+scroollMotion();
+
+// add active to selected menu link
+function linkActive() {
+  const lis = document.querySelectorAll('li');
+  for (const item of lis) {
+     item.addEventListener('click',  function() {
+     for (const i of lis) {
+      i.classList.remove('active');
+      }
+      item.classList.add('active');
+    })
+  }
+}
+linkActive();
